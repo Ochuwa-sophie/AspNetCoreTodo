a5de00e2af58a441c06245a205cf0dfbb40fec63
Ochuwa-sophie <Sophieobomighie@gmail.com>
2020-08-21 14:56:14 +0100

    Implemented Authorization

--- AspNetCoreTodo/wwwroot/js/site.js
+++ AspNetCoreTodo/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('.done-checkbox').on('click', function(e){
+        markcompleted(e.target);
+    });
+    
+});
+
+function markcompleted(checkbox) {
+    checkbox.disabled = true;
+
+    var row = checkbox.closest('tr');
+    $(row).addclass('done');
+
+    var form = checkbox.closest('form');
+    form.submit();
+    
+}
