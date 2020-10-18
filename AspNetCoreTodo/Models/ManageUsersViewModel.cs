0b2cd83081c332face1af794dc2f3206524df6be
Ochuwa-sophie <Sophieobomighie@gmail.com>
2020-10-18 07:06:14 +0100

    fixed sql issues

--- AspNetCoreTodo/Models/ManageUsersViewModel.cs
+++ AspNetCoreTodo/Models/ManageUsersViewModel.cs
@@ -8,8 +8,8 @@ namespace AspNetCoreTodo.Models
     {
         //Here i brought in the "Microsoft.AspNetCore.Identity;" namespace and changed ApplicationUser to IdentityUser.
         //This was where i had issue when i was following the little aspnet then too .. it really has been a while and i have forgoten what actually happend but i will try and find out and let you know... btw i am following my own solution hen to fix this.
-        public IdentityUser[] Administrators { get; set; }
+        public ApplicationUser[] Administrators { get; set; }
 
-        public IdentityUser[] Everyone { get; set; }
+        public ApplicationUser[] Everyone { get; set; }
     }
 }
