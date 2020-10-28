477f40abcac2a61830c08c13cca5c5423d08b146
Ochuwa-sophie <Sophieobomighie@gmail.com>
2020-10-28 12:57:14 +0100

    Created the interface and implemented it as a service

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -9,11 +9,17 @@ namespace AspNetCoreTodo.Controllers
     public class TodoController : Controller
     {
         //this action method is responsible for 
-        //1. getting the to-do items from the db
+        //1. getting the to-do items from the db. We'll use services to do this so as to keep the controller lean
         //2. putting those items into a model the view can understand
-        //3. sending the view back to the user's browser
-        
-        //using IActionResult because it gives me a wider scope of return types
+        // this I'm doing . 3. sending the view back to the user's browser/view rendering
+        //this I'm also doing . handle incoming data or input from the user
+        //business logic is handled by my service class
+
+
+        // Presentation layer = ontrollers and view 
+        // Service layer =  business logic and databse code
+      
+       //using IActionResult because it gives me a wider scope of return types
         public IActionResult Index()
         {
 
---
+++ AspNetCoreTodo/Services/ITodoItemService.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+//need to reference the model
+
+        namespace AspNetCoreTodo.Services
+        {
+            public interface ITodoItemService
+            
+            {
+                Task<TodoItem[]> GetIncompleteItemsAsync();
+            }
+            //Getincompleteitemsasync requires no parameter and returns task<todoitem[]>
+            //the task type is like a promise because getincomplete... is an async method so i may not get my todo immediately because
+            //I need to talk to the db
+        }
+
+
+
+
+//the object methods and properties are defined/signed here
+//for interacting with the todo items in the db
