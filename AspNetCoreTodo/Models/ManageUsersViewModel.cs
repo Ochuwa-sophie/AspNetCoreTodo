c7ba89ef6a09d8b351882e351a2c9d7c8784e6cf
Ochuwa-sophie <sophiecodes@gmail.com>
2021-05-11 09:29:47 +0100

    update

--- AspNetCoreTodo/Models/ManageUsersViewModel.cs
+++ AspNetCoreTodo/Models/ManageUsersViewModel.cs
@@ -1,10 +1,5 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
-<<<<<<< HEAD
-=======
-
->>>>>>> 03bf9151ed633a486e7b583651722e2a0d972290
-
 namespace AspNetCoreTodo.Models
 {
     public class ManageUsersViewModel
